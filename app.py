d51e1798d5262b59888bf3060508d2b36cdb1070
MajMarmalade <122476521+MajMarmalade@users.noreply.github.com>
2026-03-18 00:01:24 -0500

    Adding First Image Assets

--- app.py
+++ app.py
@@ -7,17 +7,33 @@ pygame.init()
 bodyfont = pygame.font.Font('VT323-Regular.ttf', 30)
 
 screen = pygame.display.set_mode((800,480))
+screen_center = screen.get_rect().center
+
+
+
+text_surface = bodyfont.render('Hello, World!', True, (255, 255, 255)) # White text
+Base_DOD_Logo  = pygame.image.load('Assets\DOD_Logo.PNG').convert_alpha()
+Large_DOD_Logo = pygame.transform.scale(Base_DOD_Logo, (240,240))
+XLarge_DOD_Logo = pygame.transform.scale(Base_DOD_Logo, (480,480))
+
+XLDOD_Rect = XLarge_DOD_Logo.get_rect()
+
+
 
 running = True
 while running: 
 
-  text_surface = bodyfont.render('Hello, World!', True, (255, 255, 255)) # White text
+
 
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
 
-  screen.blit(text_surface)
+  #screen.blit(text_surface)
+
+  #screen.blit(Base_DOD_Logo, (120,120))
+  #screen.blit(Large_DOD_Logo, (120,120))
+  screen.blit(XLarge_DOD_Logo, ((800-480)/2, 0))
 
   pygame.display.flip()
 

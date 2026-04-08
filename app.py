8a30d91a00f74d7025d5d382446ee6179c55658b
MajMarmalade <122476521+MajMarmalade@users.noreply.github.com>
2026-04-07 23:46:23 -0500

    button code and others

--- app.py
+++ app.py
@@ -2,6 +2,10 @@
 
 import pygame
 
+
+
+
+
 pygame.init()
 
 bodyfont = pygame.font.Font('VT323-Regular.ttf', 30)
@@ -11,12 +15,22 @@ screen_center = screen.get_rect().center
 
 
 
+
+# asset testing
 text_surface = bodyfont.render('Hello, World!', True, (255, 255, 255)) # White text
 Base_DOD_Logo  = pygame.image.load('Assets/DOD_Logo.PNG').convert_alpha()
 Large_DOD_Logo = pygame.transform.scale(Base_DOD_Logo, (240,240))
 XLarge_DOD_Logo = pygame.transform.scale(Base_DOD_Logo, (480,480))
 
 XLDOD_Rect = XLarge_DOD_Logo.get_rect()
+
+#loading bar spritesheet
+#LoadingBar_Sheet = pygame.image.load('Assets/Loading.PNG').convert_alpha()
+#LoadingBarFrame = LoadingBar_Sheet.images_at((0,0,200,50), (201,0,200,50),(401,0,200,50), (601,0,200,50),
+#                                            (801,0,200,50), (1001,0,200,50),(1201,0,200,50),
+#                                            (1401,0,200,50),(0,51,200,50), (201,51,200,50),(401,51,200,50))
+
+
 
 
 
@@ -35,16 +49,11 @@ while running:
   #screen.blit(Large_DOD_Logo, (120,120))
   screen.blit(XLarge_DOD_Logo, ((800-480)/2, 0))
 
+  #screen.blit(LoadingBarFrame[2])
+
+
   pygame.display.flip()
 
 pygame.quit()
-
-
-
-
-
-
-
-
 
 
---
+++ buttonListner.py
@@ -0,0 +1,71 @@
+import time
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+import pyautogui
+
+
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+
+
+
+#pin numbers for buttons
+HOME = 18
+L_ARW = 19
+D_ARW = 21
+U_ARW = 22
+R_ARW = 23
+YES = 24
+NO = 26
+TRIGGER = 29
+
+
+#Import all button
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(9, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(8, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+
+def button_callback(channel):
+    if channel == HOME:
+        print("Home Button was pushed!")
+    if channel == L_ARW:
+        print("Left Arrow Button was pushed!")
+        pyautogui.press('left')
+    if channel == D_ARW:
+        print("Down Arrow Button was pushed!")
+        pyautogui.press('down')    
+    if channel == U_ARW:
+        print("Up Arrow Button was pushed!")
+        pyautogui.press('up') 
+    if channel == R_ARW:
+        print("Right Arrow Button was pushed!")
+        pyautogui.press('right')
+    if channel == YES:
+        print("Yes Button was pushed!")
+        pyautogui.press('enter')
+    if channel == NO:
+        print("No Button was pushed!") 
+    if channel == TRIGGER:
+        print("Trigger Button was pushed!")
+    
+# Add event detection for both buttons
+GPIO.add_event_detect(HOME, GPIO.RISING, callback=button_callback, bouncetime=200)
+GPIO.add_event_detect(L_ARW, GPIO.RISING, callback=button_callback, bouncetime=200)
+GPIO.add_event_detect(D_ARW, GPIO.RISING, callback=button_callback, bouncetime=200)
+GPIO.add_event_detect(U_ARW, GPIO.RISING, callback=button_callback, bouncetime=200)
+GPIO.add_event_detect(R_ARW, GPIO.RISING, callback=button_callback, bouncetime=200)
+GPIO.add_event_detect(YES, GPIO.RISING, callback=button_callback, bouncetime=200)
+GPIO.add_event_detect(NO, GPIO.RISING, callback=button_callback, bouncetime=200)
+GPIO.add_event_detect(TRIGGER, GPIO.RISING, callback=button_callback, bouncetime=200)
+
+# Main loop
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    print("\nProgram stopped by user.\n")
